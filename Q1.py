cd82de5973c5d4fcf198096dfb1f5c05375455cf
Luiza <luizacpacheco@poli.ufrj.br>
2019-10-02 01:43:19 -0300

    Atualização - Questão 1 corrigida

--- Q1.py
+++ Q1.py
@@ -1,5 +1,5 @@
 #Questão 1
-
+import math  
 def Q1():
 	print("Para a equação ax^2 + bx + c, insira os valores de a, b e c, respectivamente.")
 	a = int(input())
@@ -9,8 +9,8 @@ def Q1():
 	x1 = 0
 	x2 = 0
 	if(Delta > 0):
-		x1 = ((b*(-1)) + sqrt(Delta))/(2*a)
-		x2 = ((b*(-1)) - sqrt(Delta))/(2*a)
+		x1 = ((b*(-1)) + math.sqrt(Delta))/(2*a)
+		x2 = ((b*(-1)) - math.sqrt(Delta))/(2*a)
 		print("As raizes da equação são", x1, "e", x2)
 		return (0)
 	elif(Delta == 0):
@@ -18,8 +18,11 @@ def Q1():
 		print("A raiz da equação é", x1)
 		return (0)
 	else:
-		print("As raizes da equação são", b, "+", math.sqrt(Delta*(-1)), "i)/", 2*a, "e"//
-			b, "-", sqrt(Delta*(-1))), "i)/", 2*a)
+                #raizDelta = math.
+		print("As raizes da equação são (", b, "+", math.sqrt(Delta*(-1)), \
+                      "i)/", 2*a, "e (",b, "-", math.sqrt(Delta*(-1)), "i)/", 2*a)
 		return 1
 		
 	i = input()
+
+Q1()
