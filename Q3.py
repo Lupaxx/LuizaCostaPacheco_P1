96b6f6ee588b60867779b30ee927e2eae0854fc2
Luiza <luizacpacheco@poli.ufrj.br>
2019-10-02 02:46:04 -0300

    Atualização - Questão 3 corrigida

--- Q3.py
+++ Q3.py
@@ -1,20 +1,16 @@
 #Questão 3
 
 def Q3( ):
-                anterior = 1
+                anterior = 10
                 n = 1
                 i = 3
                 sinal = 1
-
-
-                while((1/anterior) - (1/i) <= 5*(10**(-8))):
+                while(abs(n*sinal - anterior) > (5*(10**(-8)))):
                         sinal = sinal*(-1)
-                        if(sinal == 1):
-                                n += 1/i
-                        else:
-                                n -= 1/i
-                        anterior = i
+                        anterior = n
+                        n += (1/i)*sinal
                         i += 2
                 print(4*n)
+
 Q3()
 
